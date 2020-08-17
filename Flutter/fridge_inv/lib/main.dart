75141d40e5a1ff1b2e0409e57f385f38d4114112
admin <anthony@durrer.co.za>
2020-08-17 14:48:23 +0200

    Dyamic List Builder

--- Flutter/fridge_inv/lib/main.dart
+++ Flutter/fridge_inv/lib/main.dart
@@ -20,8 +20,42 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new ListDisplay(),
     );
+  }
+}
+
+class ListDisplay extends StatefulWidget {
+  @override
+  State createState() => new DynamicList();
+}
+
+class DynamicList extends State<ListDisplay> {
+  List<String> litems = [];
+  final TextEditingController eCtrl = new TextEditingController();
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Dynamic Demo"),
+        ),
+        body: new Column(
+          children: <Widget>[
+            new TextField(
+                controller: eCtrl,
+                onSubmitted: (text) {
+                  litems.add(text);
+                  eCtrl.clear();
+                  setState(() {});
+                }),
+            new Expanded(
+                child: new ListView.builder(
+                    itemCount: litems.length,
+                    itemBuilder: (BuildContext ctxt, int Index) {
+                      return new Text(litems[Index]);
+                    }))
+          ],
+        ));
   }
 }
 
